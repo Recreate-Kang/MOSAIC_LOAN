4159a13a5da40591424982288edd8ff104ad9dcb
recreate.kang <recreate.kang@gmail.com>
2025-04-04 09:39:44 +0900

    fiq: querydsl gradle의존성 annotationProcessor수정

--- back/contract-service/src/main/java/com/mosaic/loan/service/LoanServiceImpl.java
+++ back/contract-service/src/main/java/com/mosaic/loan/service/LoanServiceImpl.java
@@ -19,6 +19,22 @@ public class LoanServiceImpl implements LoanService {
     private final LoanKafkaProducer loanKafkaProducer;
     private final LoanRepository loanRepository;
     private final InternalApiClient internalApiClient;
+    //TODO 대출 실행하기
+    //api : 신용평가 api
+    //pub : 대출신청 발행
+
+
+    //sub : 빌려줄 돈 모금 로직
+    //approve : 모금이 가능한지 검증
+    //execute: 데이터베이스에 해당 사항 반영
+    //pub : 대출 모금 금액 계좌로 인출
+
+
+    //TODO 빌린돈 인출하기
+    //status: 대출 상태 실행으로 변경
+    //sub : 인출 확인 트랜젝션
+    //pub : 완료 ACK 발행
+
 
     @Override
     public void createLoan(CreateLoanRequestDto request) throws JsonProcessingException {
@@ -38,4 +54,12 @@ public class LoanServiceImpl implements LoanService {
     public void completeLoan() {
         //Todo loanConumser에서 완료 수신 후 loan증가
     }
+
+    //TODO 돈 갚기(스케쥴러)
+    //scheduele: 돈 갚는 계약
+    //pub: 계좌기준으로 kafka 이벤트 발행
+    //sub: 계좌에 돈 입금 확인
+    //transaction: 이자 및 금액 분배
+
+    //TODO 돈 갚기(조기상환)
 }
